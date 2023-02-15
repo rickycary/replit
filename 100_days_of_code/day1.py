e3a7fb427ed13cbb2e38f7762a7f3d7ba0602a21
Richard <ricky.cary1@gmail.com>
2023-02-15 10:57:41 -0600

    100 Days of Code: Day 1

---
+++ 100_days_of_code/day1.py
@@ -0,0 +1,6 @@
+print("""Ricky
+      February 15th 
+      I am signing up for Replit's 100 days of Python challenge!
+      I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day. I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!
+      You can follow my progress at replit.com/rickycary
+      """)
